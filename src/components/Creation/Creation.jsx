faa7d9f8793e7048f59fc0897fa5bf67fbe6fc80
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-10-09 21:57:11 +0200

    added hover effects

--- src/components/Creation/Creation.jsx
+++ src/components/Creation/Creation.jsx
@@ -3,7 +3,8 @@ import './Creation.css';
 
 function Creation({title, image}) {
   return (
-    <div className="creation" id="creation-earth" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${image})`}}>        
+    <div className="creation" id="creation-earth" >        
+        <div className="creation__img" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${image})`}}></div>
         <p className='creation__title'>{title}</p>
       </div>
   )
